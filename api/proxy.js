e778e890412fe6f9b25c50b9f1df4af94d085640
Sam <140335136+SamJ2104@users.noreply.github.com>
2024-03-15 22:13:26 +0000

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,12 +3,15 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (req, res) => {
   let target = "https://www.crazygames.com/"; // Your website URL
 
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: (path, req) => {
-      // Rewrite the path to include the target URL
-      return path.replace(/^\/(.*)/, `${target}$1`);
-    },
-  })(req, res);
+  // Check if the request is made through a link click
+  if (req.headers.referer && req.headers.referer.startsWith(target)) {
+    // Intercept only if the referer is from the target website
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    })(req, res);
+  } else {
+    // If not from a link click, pass the request through
+    res.end();
+  }
 };
