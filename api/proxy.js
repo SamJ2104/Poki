0bc3bf04b2dd993ccf8c900ce8d63e767c1cf117
Sam <140335136+SamJ2104@users.noreply.github.com>
2024-03-15 22:09:46 +0000

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,14 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.crazygames.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
+  let target = "https://www.crazygames.com/"; // Your website URL
 
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
+    pathRewrite: (path, req) => {
+      // Rewrite the path to include the target URL
+      return path.replace(/^\/(.*)/, `${target}$1`);
     },
   })(req, res);
 };
